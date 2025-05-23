06bfe8009f25d88b9ec97e9ac2fb32bfcf8804d2
Itamar Junior <hello@itjunior.dev>
2025-05-24 00:17:27 +0100

    Simplify Number facade import by using class alias in payment link view

--- resources/views/livewire/payment/payment-link-index.blade.php
+++ resources/views/livewire/payment/payment-link-index.blade.php
@@ -28,7 +28,7 @@
                             {{ $link->title }}
                         </h3>
                         <p class="text-sm text-gray-600 dark:text-gray-400">
-                            {{ Illuminate\Support\Facades\Number::currency($link->amount, $link->business->currency) }} &middot;
+                            {{ Number::currency($link->amount, $link->business->currency) }} &middot;
                             {{ $link->sales_count }} sales
                         </p>
                     </div>
